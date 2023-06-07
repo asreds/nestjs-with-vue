d6f6c31fd72781b3414c469a7d5281e674cfccb5
Asih Asreds <asreds@Asihs-Mac-Pro.local>
2023-06-07 15:16:04 +0700

    create sample nets routing and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static'; // New
+import { CatsModule } from './cats/cats.module';
 import { join } from 'path'; // New
 
 @Module({
@@ -10,6 +11,7 @@ import { join } from 'path'; // New
       // New
       rootPath: join(__dirname, '..', '/client/dist'), // New
     }), // New
+    CatsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
